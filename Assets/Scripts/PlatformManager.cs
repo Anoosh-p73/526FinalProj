4246c94178327431876cf63d03bf585f17ab9faa
Anoosh K <55269680+Anoosh-p73@users.noreply.github.com>
2024-02-21 17:46:07 -0800

    Added relevant packages and new scripts

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameInitializer : MonoBehaviour {
+public class PlatformManager : MonoBehaviour {
 
     [SerializeField] private GameObject LevelParent;
 
+    private List<GameObject> Platforms;
+
     // Start is called before the first frame update
     void Start() {
+
+        Platforms = new List<GameObject>();
+
         if (LevelParent != null) {
             AddScriptToChildren(LevelParent);
         }
@@ -18,9 +23,24 @@ public class GameInitializer : MonoBehaviour {
             // Check the tag of each child
             if (child.CompareTag("Stationary")) {
                 child.gameObject.AddComponent<StationaryPlatformScript>();
+                Platforms.Add(child.gameObject);
             } else if (child.CompareTag("Moving")) {
                 child.gameObject.AddComponent<MovingPlatformScript>();
+                Platforms.Add(child.gameObject);
             }
         }
     }
+
+    void ResetPlatforms() {
+        foreach (GameObject platform in Platforms) {
+            // Check the tag of each child
+            if (platform.CompareTag("Stationary")) {
+                platform.GetComponent<StationaryPlatformScript>().Reset();
+            } else if (platform.CompareTag("Moving")) {
+                platform.GetComponent <MovingPlatformScript>().Reset();
+            }
+        }
+    }
+
+
 }
